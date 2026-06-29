74e67701e3bdefb0aad13bbd96c141c9b34c671f
Xldln <yubinux@yeah.net>
2026-06-29 10:14:49 +0800

    del cache

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
-const API_BASE = 'http://localhost:9000/micropixels'
+
+const API_BASE = import.meta.env.DEV
+  ? 'http://localhost:9000/micropixels'       
+  : 'https://api.yourdomain.com/micropixels'  
 
 export default API_BASE
